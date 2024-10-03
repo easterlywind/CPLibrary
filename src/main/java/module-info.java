c4bd44b777ba43baf6feeb58c43ce9c428f538cd
nthai <161859471+easterlywind@users.noreply.github.com>
2024-10-03 15:16:08 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.cplibrary {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.cplibrary to javafx.fxml;
+    exports com.example.cplibrary;
+}
